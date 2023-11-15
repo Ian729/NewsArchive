7bf354725399ad81c9c6cfa7b58af1d21bb657f6
Ian729 <438410248@qq.com>
2023-11-15 22:55:23 +0800

    try 3 news

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # NewsArchive
 Auto RSS New York Times and Generate Word Cloud
+
+## New York Times Global News
+![Global](./image.png)
+
+## New York Times United States News
+![Global](./usnews.png)
+
+## New York Times Asia News
+![Global](./asia.png)
--- rsscloud.py
+++ rsscloud.py
@@ -1,10 +1,27 @@
 import feedparser
 from wordcloud import WordCloud
+# World News
 ents = feedparser.parse("https://rss.nytimes.com/services/xml/rss/nyt/World.xml").entries
 words = ""
 for ent in ents:
     words += ent.get("content", [{}])[0].get('value', "")
-# Construct the word cloud
 wordcloud = WordCloud(width=800, height=400).generate(words)
 wordcloud.to_file("image.png")
+
+# US News
+ents = feedparser.parse("https://rss.nytimes.com/services/xml/rss/nyt/US.xml").entries
+words = ""
+for ent in ents:
+    words += ent.get("content", [{}])[0].get('value', "")
+wordcloud = WordCloud(width=800, height=400).generate(words)
+wordcloud.to_file("usnews.png")
+
+# Asian News
+ents = feedparser.parse("https://rss.nytimes.com/services/xml/rss/nyt/AsiaPacific.xml").entries
+words = ""
+for ent in ents:
+    words += ent.get("content", [{}])[0].get('value', "")
+wordcloud = WordCloud(width=800, height=400).generate(words)
+wordcloud.to_file("asia.png")
+
 print("Done!")
