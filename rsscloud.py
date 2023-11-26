e6b4acb164778b7e12223c54a41dfd0d0255ce55
Ian729 <438410248@qq.com>
2023-11-27 00:05:28 +0800

    [feat] update news summary in README markdown as well

--- rsscloud.py
+++ rsscloud.py
@@ -1,27 +1,54 @@
 import feedparser
 from wordcloud import WordCloud
-# World News
+# Global News
 ents = feedparser.parse("https://rss.nytimes.com/services/xml/rss/nyt/World.xml").entries
-words = ""
-for ent in ents:
-    words += ent.get("content", [{}])[0].get('value', "")
-wordcloud = WordCloud(width=800, height=400).generate(words)
-wordcloud.to_file("image.png")
+global_news = ""
+for ent in ents[:10]:
+    global_news += "* "
+    global_news += ent.get("summary","")
+    global_news += "\n"
+wordcloud = WordCloud(width=800, height=400).generate(global_news)
+wordcloud.to_file("global.png")
 
 # US News
 ents = feedparser.parse("https://rss.nytimes.com/services/xml/rss/nyt/US.xml").entries
-words = ""
-for ent in ents:
-    words += ent.get("content", [{}])[0].get('value', "")
-wordcloud = WordCloud(width=800, height=400).generate(words)
+us_news = ""
+for ent in ents[:10]:
+    us_news += "* "
+    us_news += ent.get("summary","")
+    us_news += "\n"
+wordcloud = WordCloud(width=800, height=400).generate(us_news)
 wordcloud.to_file("usnews.png")
 
 # Asian News
 ents = feedparser.parse("https://rss.nytimes.com/services/xml/rss/nyt/AsiaPacific.xml").entries
-words = ""
-for ent in ents:
-    words += ent.get("content", [{}])[0].get('value', "")
-wordcloud = WordCloud(width=800, height=400).generate(words)
-wordcloud.to_file("asia.png")
+asian_news = ""
+for ent in ents[:10]:
+    asian_news += "* "
+    asian_news += ent.get("summary","")
+    asian_news += "\n"
+wordcloud = WordCloud(width=800, height=400).generate(asian_news)
+wordcloud.to_file("asian.png")
+print("update markdown file")
+
+md = open("./README.md", 'w')
+md.write("# NewsArchive\n")
+md.write("Auto RSS New York Times and Generate Word Cloud\n")
+md.write("\n")
+md.write("## New York Times Global News\n")
+md.write(global_news)
+md.write("\n")
+md.write("![Global](./global.png)")
+md.write("\n")
+md.write("## New York Times United States News\n")
+md.write(us_news)
+md.write("\n")
+md.write("![US](./usnews.png)")
+md.write("\n")
+md.write("## New York Times Asia News\n")
+md.write(asian_news)
+md.write("\n")
+md.write("![Asian](./asian.png)")
+md.write("\n")
 
 print("Done!")
