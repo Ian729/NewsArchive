e9dc5612e37ed7443e68ffd095b73f8b0686f078
Ian729 <438410248@qq.com>
2024-08-17 00:08:40 +0800

    use black and isort to format data

--- rsscloud.py
+++ rsscloud.py
@@ -1,11 +1,14 @@
 import feedparser
 from wordcloud import WordCloud
+
 # Global News
-ents = feedparser.parse("https://rss.nytimes.com/services/xml/rss/nyt/World.xml").entries
+ents = feedparser.parse(
+    "https://rss.nytimes.com/services/xml/rss/nyt/World.xml"
+).entries
 global_news = ""
 for ent in ents[:10]:
     global_news += "* "
-    global_news += ent.get("summary","")
+    global_news += ent.get("summary", "")
     global_news += "\n"
 wordcloud = WordCloud(width=800, height=400).generate(global_news)
 wordcloud.to_file("global.png")
@@ -15,23 +18,25 @@ ents = feedparser.parse("https://rss.nytimes.com/services/xml/rss/nyt/US.xml").e
 us_news = ""
 for ent in ents[:10]:
     us_news += "* "
-    us_news += ent.get("summary","")
+    us_news += ent.get("summary", "")
     us_news += "\n"
 wordcloud = WordCloud(width=800, height=400).generate(us_news)
 wordcloud.to_file("usnews.png")
 
 # Asian News
-ents = feedparser.parse("https://rss.nytimes.com/services/xml/rss/nyt/AsiaPacific.xml").entries
+ents = feedparser.parse(
+    "https://rss.nytimes.com/services/xml/rss/nyt/AsiaPacific.xml"
+).entries
 asian_news = ""
 for ent in ents[:10]:
     asian_news += "* "
-    asian_news += ent.get("summary","")
+    asian_news += ent.get("summary", "")
     asian_news += "\n"
 wordcloud = WordCloud(width=800, height=400).generate(asian_news)
 wordcloud.to_file("asian.png")
 print("update markdown file")
 
-md = open("./README.md", 'w')
+md = open("./README.md", "w")
 md.write("# NewsArchive\n")
 md.write("Auto RSS New York Times and Generate Word Cloud\n")
 md.write("\n")
